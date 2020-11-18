7630868f4ce88f1a3bc6b636c426961dac416d64
nwoo0 <nicholastcw2022@student.cis.edu.hk>
2020-11-18 10:20:34 +0800

    added constant variables for other commands

--- src/main/java/com/cis/Utils/Constants.java
+++ src/main/java/com/cis/Utils/Constants.java
@@ -5,9 +5,22 @@ public class Constants {
 
     // Paths
     public static final String GET_USER = "/getUser";
+    public static final String AUTH = "/authenticate";
+    public static final String GET_POST = "/getPost";
+    public static final String GET_POSTS = "/getPosts";
+    public static final String RATE_POST = "/ratePost";
+    public static final String GET_POST_POINTS = "/getPostPoints";
+    public static final String GET_USER_POINTS = "/getUserPoints";
+    public static final String SPEND_POINTS = "/spendPoints";
+    public static final String GET_DISCOUNTS = "/getDiscounts";
 
     // Params
     public static final String ID_PARAM = "id";
+
+
+    // Status
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
 
     // Error Messages
     public static final String JSONError = "JSON Error";
