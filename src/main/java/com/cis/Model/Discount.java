0e65bec6af484b74f1c16ac1e6d6c37d519e5c66
nwoo0 <nicholastcw2022@student.cis.edu.hk>
2020-11-18 09:56:38 +0800

    add instance variables

--- src/main/java/com/cis/Model/Discount.java
+++ src/main/java/com/cis/Model/Discount.java
@@ -1,2 +1,53 @@
-package com.cis.Model;public class Discount {
+package com.cis.Model;
+
+public class Discount {
+    private String id, userId, storeName, name, text;
+
+    public Discount(String id, String userId, String storeName, String name, String text) {
+        this.id = id;
+        this.userId = userId;
+        this.storeName = storeName;
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
