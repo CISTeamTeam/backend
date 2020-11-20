34854019cafd972c82e7b67f410f432bed7f6a0f
nwoo0 <nicholastcw2022@student.cis.edu.hk>
2020-11-20 21:54:54 +0800

    fix userid

--- src/main/java/com/cis/Controller/ServerController.java
+++ src/main/java/com/cis/Controller/ServerController.java
@@ -255,7 +255,7 @@ public class ServerController implements HTTPServerListener {
         String id = (String) request.getParam(Constants.ID_PARAM);
 
         int points = data.getUsers().get(id).getPoints();
-        return "{ \"userID\": " + id + ", \"points\": " + points + " }";
+        return "{ \"id\": " + "\"" + id + "\"" + ", \"points\": " + points + " }";
     }
 
     private String spendPoints(Request request) {
